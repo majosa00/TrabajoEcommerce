9acfa2645cc9db2e4664eb8e4738ab4135063e97
JosekaMT <116631489+JosekaMT@users.noreply.github.com>
2024-01-28 14:37:07 +0100

    template

--- resources/views/template/home.blade.php
+++ resources/views/template/home.blade.php
@@ -16,7 +16,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img src="../../img/energeticwave-logo.png" alt="Logotipo" width="200" class="d-inline-block align-text-top">
+                <img src="../img/energeticwave-logo.png" alt="Logotipo" width="200" class="d-inline-block align-text-top">
 
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
