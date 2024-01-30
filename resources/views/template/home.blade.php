5bf4723e9bf94a82321a205396331eeb99545a83
JosekaMT <116631489+JosekaMT@users.noreply.github.com>
2024-01-30 15:32:20 +0100

    home

--- resources/views/template/home.blade.php
+++ resources/views/template/home.blade.php
@@ -18,7 +18,6 @@
             <a class="navbar-brand" href="#">
                 <img src="../../img/energeticwave-logo" alt="Logotipo" width="200"
                     class="d-inline-block align-text-top">
-
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
