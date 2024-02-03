e6add50e4a32ac854e413c09a6919b77d61ac00c
martaboso98 <frboruz@alumnos.ilernasevilla.com>
2024-02-03 23:33:21 +0100

    card

--- resources/views/logged.blade.php
+++ resources/views/logged.blade.php
@@ -1,7 +1,8 @@
 @extends('layaouts.app')
 
 @section('content')
-    <div class="container p-5">
+    {{-- <div class="container p-5">
+
         <body>
             <h1 class="mb-3">PRODUCTS</h1>
             <table class="table table-responsive">
@@ -24,5 +25,25 @@
                     </tr>
                 @endforeach
             </table>
+    </div> --}}
+
+    <div class="container p-5">
+        <h1 class="mb-3">PRODUCTS</h1>
+        <div class="cards_inner">
+            @foreach ($products as $product)
+                <div class="col-lg-4">
+                    <div class="card">
+                        <h4>{{ $product->name }}</h4>
+                        <img src="{{ asset('images/redbull.jpg') }}" alt="" class="card-img-top">
+                        <p>{{ $product->description }}</p>
+                        <p class="price-container">{{ $product->udpack }} uds. | {{ $product->price }} $</p>
+                        <form action="{{ route('cart.addToCart', $product->id) }}" method="POST">
+                            @csrf
+                            <button class="btn btn-warning btn-sm" type="submit">Buy</button>
+                        </form>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
--- resources/views/orderadmin.blade.php
+++ resources/views/orderadmin.blade.php
@@ -1,49 +1,27 @@
-<<<<<<< HEAD
-<!-- @ extends('auth.template')    Antiguo navbar-->
-
-@extends('layaouts.app')
-
-@section('content')
-    <div class="container">
-        <h1>PEDIDOS</h1>
-=======
 @extends('layaouts.app2')
 
 @section('content')
     <div class="container p-5">
         <h1 class="mb-3">ORDERS</h1>
->>>>>>> origin/main
         <table class="table table-responsive">
             <thead>
                 <tr>
                     <th>ID User</th>
                     <th>ID Order</th>
-<<<<<<< HEAD
-=======
                     <th>Order Date</th>
                     <th>Total Price</th>
->>>>>>> origin/main
                     <th>State</th>
                 </tr>
             </thead>
             @foreach ($orders as $order)
                 <tr>
-<<<<<<< HEAD
-                    <td>{{ $order->id_user }}</td>
-                    <td>{{ $order->id }}</td>
-=======
                     <td>{{ $order->user_id }}</td>
                     <td>{{ $order->id }}</td>
                     <td>{{ $order->orderDate }}</td>
                     <td>{{ $order->totalPrice }} $</td>
->>>>>>> origin/main
                     <td>{{ $order->state }}</td>
                 </tr>
             @endforeach
         </table>
-<<<<<<< HEAD
-    @endsection
-=======
     </div>
 @endsection
->>>>>>> origin/main
