d2184c2d1fccc36700ca5e41e0609ba5fafd2dcf
martaboso98 <frboruz@alumnos.ilernasevilla.com>
2024-02-04 11:54:31 +0100

    cards más guays

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -17,7 +17,7 @@ class OrderController extends Controller
 
     public function createOrder($cartId, $productId, $totalprice)
     {
-
+        
     }
 
     public function showOrder()
--- resources/views/logged.blade.php
+++ resources/views/logged.blade.php
@@ -1,46 +1,33 @@
 @extends('layaouts.app')
 
 @section('content')
-    {{-- <div class="container p-5">
-
-        <body>
-            <h1 class="mb-3">PRODUCTS</h1>
-            <table class="table table-responsive">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Description</th>
-                    </tr>
-                </thead>
-                @foreach ($products as $product)
-                    <tr>
-                        <td>{{ $product->name }}</td>
-                        <td>{{ $product->description }}</td>
-                        <td>
-                            <form action="{{ route('cart.addToCart', $product->id) }}" method="POST">
-                                @csrf
-                                <button class="btn btn-warning btn-sm" type="submit">Buy</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </table>
-    </div> --}}
-
-    <div class="container p-5">
+    <div class="container mt-4">
         <h1 class="mb-3">PRODUCTS</h1>
-        <div class="cards_inner">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
             @foreach ($products as $product)
-                <div class="col-lg-4">
-                    <div class="card">
-                        <h4>{{ $product->name }}</h4>
-                        <img src="{{ asset('images/redbull.jpg') }}" alt="" class="card-img-top">
-                        <p>{{ $product->description }}</p>
-                        <p class="price-container">{{ $product->udpack }} uds. | {{ $product->price }} $</p>
-                        <form action="{{ route('cart.addToCart', $product->id) }}" method="POST">
-                            @csrf
-                            <button class="btn btn-warning btn-sm" type="submit">Buy</button>
-                        </form>
+                <div class="col">
+                    <div class="card h-100 text-white bg-dark">
+                        <img src="{{ asset('images/redbull.jpg') }}" class="card-img-top" alt="{{ $product->name }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $product->name }}</h5>
+                            <p class="card-text">{{ $product->description }}</p>
+                            <p class="card-text">{{ $product->udpack }} uds. | {{ $product->price }} $</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <form action="{{ route('cart.addToCart', $product->id) }}" method="POST"
+                                    class="d-flex justify-content-start">
+                                    @csrf
+                                    <button class="btn btn-warning" type="submit"><i class="fas fa-shopping-cart"></i> Add
+                                        to
+                                        Cart</button>
+                                </form>
+                                <!-- Mientras, pongo esa ruta para que no me de fallo -->
+                                <form action="{{ route('cart.addToCart', $product->id) }}" method="POST"
+                                    class="d-flex justify-content-end">
+                                    @csrf
+                                    <button class="btn btn-danger" type="submit"><i class="fas fa-heart"></i></button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
             @endforeach
