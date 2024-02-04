47835777843a4873947b538ad15a8385735a2f5f
martaboso98 <frboruz@alumnos.ilernasevilla.com>
2024-02-04 13:19:01 +0100

    carrito funciona por fin bien

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -15,25 +15,23 @@ class OrderController extends Controller
         return view('/orderadmin')->with('orders', $orders);
     }
 
-    public function createOrder($cartId, $productId, $totalprice)
-    {
-        
-    }
-
     public function showOrder()
     {
-        //Obtener el usuario autenticado actualmente
         $userId = Auth::id();
 
-        //Solo los pedidos del usuario autenticado
-        $ordersByID = Order::where('user_id', $userId)->get();
-
-        //Obtener el carrito del usuario
+        // Obtener el carrito del usuario
         $cart = Cart::where('user_id', $userId)->first();
 
-        //Obtener los IDs de productos en el carrito
-        $productIds = $cart->products()->pluck('product_id')->toArray();
+        // Verificar si el usuario tiene un carrito
+        if (!$cart) {
+            // Manejar el caso en que el usuario no tiene carrito
+            return view('order', ['orders' => []]);
+        }
 
-        return view('order', ['orders' => $ordersByID, 'productIds' => $productIds]);
+        // Obtener los pedidos del usuario con productos asociados
+        $orders = Order::where('user_id', $userId)->with('products')->get();
+
+        return view('order', ['orders' => $orders]);
     }
+
 }
