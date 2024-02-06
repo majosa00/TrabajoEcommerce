4d0dc00df0e2605a5f4674bfa7a4b7f8114f2f49
martaboso98 <frboruz@alumnos.ilernasevilla.com>
2024-02-06 18:43:40 +0100

    carrito cambiar diseño

--- resources/views/products/cart.blade.php
+++ resources/views/products/cart.blade.php
@@ -4,40 +4,39 @@
 @section('content')
     <div class="container p-5">
         <h1 class="mb-3">CART</h1>
-        <table class="table table-responsive">
-            <thead>
-                <tr>
-                    <th>Product</th>
-                    <th>Description</th>
-                    <th>Amount</th>
-                    <th>Price</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($products as $product)
-                    <tr>
-                        <td>{{ $product->name }}</td>
-                        <td>{{ $product->description }}</td>
-                        <td>{{ $product->pivot->amount }}</td>
-                        <td>{{ $product->price }} $</td>
-                        <td>
-                            <form action="{{ route('cart.remove', $product->id) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger btn-sm">Remove</button>
-                            </form>
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
+        @foreach ($products as $product)
+            <div class="row">
+                <div class="col-lg-4">
+                    <img>
+                </div>
+                <div class="col-lg-4">
+                    <h3>{{ $product->name }}</h3>
+                    <p>{{ $product->description }}</p>
+                    <p>{{ $product->pivot->amount }}</p>
+                </div>
+                <div class="col-lg-4">
+                    <h3>{{ $product->price }} $</h3>
+                    <h3>Total Price:
+                        {{ $products->sum(function ($product) {
+                            return $product->price * $product->pivot->amount;
+                        }) }}
+                        $
+                    </h3>
+                </div>
+            </div>
+            <td>
+                <form action="{{ route('cart.remove', $product->id) }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger btn-sm">Remove</button>
+                </form>
+            </td>
+            </tr>
+        @endforeach
+        </tbody>
         </table>
 
-        <h3>Total Price:
-            {{ $products->sum(function ($product) {
-                return $product->price * $product->pivot->amount;
-            }) }} $
-        </h3>
+        
         <form action="{{ route('cart.pay') }}" method="POST" class="mt-3">
             @csrf
             <button class="btn btn-warning btn-sm" type="submit">Payment</button>
