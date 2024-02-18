db65ac9b17000a15bce7801cc63f46bc52be1515
Samuel Castro <dreift133@gmail.com>
2024-02-18 19:43:14 +0100

    cambios en alert

--- resources/views/products/cart.blade.php
+++ resources/views/products/cart.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container p-5">
-        @if (session('success'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-                {{ session('success') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        @endif
+<div class="container p-5">
+    @if (session('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
 
-        @if (session('mensaje'))
-            <div class="alert alert-info alert-dismissible fade show" role="alert">
-                {{ session('mensaje') }}
-                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-        @endif
+    @if (session('mensaje'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('mensaje') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
 
   
 
