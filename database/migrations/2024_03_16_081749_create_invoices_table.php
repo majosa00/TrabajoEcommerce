9e299c6231d99e231b44ec3225c1f664de20ac88
martaboso98 <frboruz@alumnos.ilernasevilla.com>
2024-02-28 20:09:44 +0100

    quitar un atributo que no hacee nada

--- database/migrations/2024_03_16_081749_create_invoices_table.php
+++ database/migrations/2024_03_16_081749_create_invoices_table.php
@@ -14,8 +14,6 @@ return new class extends Migration
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
 
-            $table->unsignedBigInteger('orders_id');
-
             $table->unsignedBigInteger('order_id')->unique();
             $table->foreign('order_id')
                     ->references('id')
