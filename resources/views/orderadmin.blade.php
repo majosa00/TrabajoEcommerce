f6da370bf064e79a7a9f4d60c3eda019d3e6b01d
martaboso98 <frboruz@alumnos.ilernasevilla.com>
2024-02-29 13:57:23 +0100

    creo que todo es responsivo por fin, y puedes cambiar imagen de marca

--- resources/views/orderadmin.blade.php
+++ resources/views/orderadmin.blade.php
@@ -2,31 +2,35 @@
 
 @section('content')
     <div class="container p-5">
-        <h1 class="mb-3">ORDERS</h1>
-        <table class="table table-responsive">
-            <thead>
-                <tr>
-                    <th>ID User</th>
-                    <th>User</th>
-                    <th>Address</th>
-                    <th>ID Order</th>
-                    <th>Order Date</th>
-                    <th>Total Price</th>
-                    <th>State</th>
-                </tr>
-            </thead>
-            @foreach ($orders as $order)
-                <tr>
-                    <td>{{ $order->user_id }}</td>
-                    <td>{{ $order->user }}</td>
-                    <td>{{ $order->address }}</td>
-                    <td>{{ $order->id }}</td>
-                    <td>{{ $order->orderDate }}</td>
-                    <td>{{ $order->totalPrice }} $</td>
-                    <td>{{ $order->state }}</td>
-                </tr>
-            @endforeach
-        </table>
+        <section id="orderadmin">
+            <h1 class="mb-3">ORDERS</h1>
+            <div class="table-responsive">
+                <table class="table table-hover stylish-table">
+                    <thead>
+                        <tr>
+                            <th>ID User</th>
+                            <th>User</th>
+                            <th>Address</th>
+                            <th>ID Order</th>
+                            <th>Order Date</th>
+                            <th>Total Price</th>
+                            <th>State</th>
+                        </tr>
+                    </thead>
+                    @foreach ($orders as $order)
+                        <tr>
+                            <td>{{ $order->user_id }}</td>
+                            <td>{{ $order->user }}</td>
+                            <td>{{ $order->address }}</td>
+                            <td>{{ $order->id }}</td>
+                            <td>{{ $order->orderDate }}</td>
+                            <td>${{ $order->totalPrice }}</td>
+                            <td>{{ $order->state }}</td>
+                        </tr>
+                    @endforeach
+            </div>
+            </table>
+        </section>
     </div>
     <div id="main-container"></div>
 @endsection
